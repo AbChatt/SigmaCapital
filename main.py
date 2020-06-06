e40872a2fe84f1c5fce9c2fd2cccdcd468bf410a
aditya101099 <aditya101099@users.noreply.github.com>
2020-06-06 22:56:45 +0100

    Revert "V 1.1"
    This reverts commit 74b357cada628c5647b7fd23453a0c487be9b083.

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
 from model import *
 
-# print(modelSelection('AAPL_Jun_2019_2020.csv', "AAPL"))
+print(modelSelection('AAPL_Jun_2019_2020.csv', "AAPL"))
 
-def highlights(ratings):
-	buy_and_sell = sorted(ratings, key=lambda x: x[0])
-	buy = sorted(list(filter(lambda x: x[0] == "BUY", buy_and_sell)), key=lambda x: x[1])
-	sell = sorted(list(filter(lambda x: x[0] == "SELL", buy_and_sell)), key=lambda x: x[1])
-	return (list(reversed(buy[-5:])), list(sell[0:5]))
-
-# print(highlights([("SELL", -1, "GOOGL"), ("SELL", -0.5, "RDSA"), ("BUY", 100, "APPL"), ("BUY", 80, "BP"), ("SELL", -0.3, "XXON"), ("BUY", 70, "MSFT"), ("BUY", 60, "BRK.A"), ("BUY", 50, "JPM"), ("SELL", -0.4, "CMCSA"), ("SELL", -0.2, "PM")]))
