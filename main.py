e22840cf8baeb2ee037988c6af9126a5e934303a
aditya101099 <aditya101099@users.noreply.github.com>
2020-06-07 02:14:58 +0100

    V 1.3
    Check changelog

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from model import *
 
-print(modelSelection('AAPL_Jun_2019_2020.csv', "AAPL"))
+# print(modelSelection('AAPL_Jun_2019_2020.csv', "AAPL"))
+# print(modelSelection('AAPL-2.csv', "AAPL"))
 
-def highlights(ratings):
+def highlights(ratings, rating):
 	buy_and_sell = sorted(ratings, key=lambda x: x[0])
 	buy = sorted(list(filter(lambda x: x[0] == "BUY", buy_and_sell)), key=lambda x: x[1])
 	sell = sorted(list(filter(lambda x: x[0] == "SELL", buy_and_sell)), key=lambda x: x[1])
-	return (list(reversed(buy[-5:])), list(sell[0:5]))
+	our_highlights = (list(reversed(buy[-5:])), list(sell[0:5]))
+	if rating == "BUY":
+		return [x[3] for x in our_highlights[0]]
+	else:
+		return [x[3] for x in our_highlights[1]]
 
-# print(highlights([("SELL", -1, "GOOGL"), ("SELL", -0.5, "RDSA"), ("BUY", 100, "APPL"), ("BUY", 80, "BP"), ("SELL", -0.3, "XXON"), ("BUY", 70, "MSFT"), ("BUY", 60, "BRK.A"), ("BUY", 50, "JPM"), ("SELL", -0.4, "CMCSA"), ("SELL", -0.2, "PM")]))
+
+print(modelSelection("DIS"))
+# print(highlights([("SELL", -1, 1, "GOOGL"), ("SELL", -0.5, 1, "RDSA"), ("BUY", 100, 1, "APPL"), ("BUY", 80, 1, "BP"), ("SELL", -0.3, 1, "XXON"), ("BUY", 70, 1, "MSFT"), ("BUY", 60, 1, "BRK.A"), ("BUY", 50, 1, "JPM"), ("SELL", -0.4, 1, "CMCSA"), ("SELL", -0.2, 1, "PM")], "SELL"))
--- model.py
+++ model.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-from pylab import rcParams 
+from pylab import rcParams
 from datetime import datetime
 from statsmodels.tsa.stattools import adfuller
 from statsmodels.tsa.seasonal import seasonal_decompose
@@ -13,8 +13,11 @@ from statsmodels.tsa.arima_model import ARIMA
 from statsmodels.tsa.holtwinters import ExponentialSmoothing
 
 # needed to pip install pmdarima - which also fetched statsmodels
-from pmdarima.arima import auto_arima     
+from pmdarima.arima import auto_arima
 from sklearn.metrics import mean_squared_error, mean_absolute_error
+
+# import API
+from alpha_vantage.timeseries import TimeSeries
 
 
 
@@ -23,13 +26,13 @@ def rating(prediction):
     number_of_days = len(prediction) # How many days are in our test set
     initial_price = prediction['Prediction'].iloc[0] # Variable names for these
     final_price = prediction['Prediction'].iloc[-1] # are self explanatory
-    model_slope = (prediction['Prediction'].iloc[-1] - initial_price)/number_of_days # Basic linear regression 
-    
+    model_slope = (prediction['Prediction'].iloc[-1] - initial_price)/number_of_days # Basic linear regression
+
     if model_slope > 0:
         return ["BUY", model_slope]
     else:
         return ["SELL", model_slope]
-    
+
 # Determine if timeseries is stationary
 def test_stationarity(timeseries):
     # Determining rolling statistics
@@ -47,239 +50,159 @@ def test_stationarity(timeseries):
     # running Augmented Dickey Fuller Test
     # print("Results of Dickey Fuller test")
     adft = adfuller(timeseries, autolag='AIC')
-    
+
     # adfuller gives output without labels - need to manually write what values mean using for loop
     output = pd.Series(adft[0:4], index=['Test Statistics', 'p-value', 'No. of lags used', 'Number of observations used'])
     for key, values in adft[4].items():
         output['critical value (%s)'%key] = values
-    
+
     test_stats = output[0] # Test statistics
     critical_values = output[4] # Critical values
-    
+
     if test_stats > critical_values: # If the test statistics are more than the critical values, the null hypothesis passes and it is not stationary
         return False
     else:
         return True # The null hypothesis IS stationary
-        
-def generateCloseModel(data, ticker):
-    
-    # inserts an entry into list of warning filters
-    warnings.filterwarnings('ignore')   
-    
-    # style of graph being plotted - font style and related preferences (can be changed)
-    plt.style.use('dark_background')
-    
-    # changing default figsize parameter
-    rcParams['figure.figsize'] = 10, 6
-    
-    data_frame = pd.read_csv(data)    # modified from example as path was not being picked up
-    
-    # gets Date column data
-    con = data_frame['Date']
-    # converts date column to a Python DateTime object
-    data_frame['Date'] = pd.to_datetime(data_frame['Date'])
-    
-    # sets data_frame index to Date column
-    data_frame.set_index('Date', inplace=True)
-    
-    # get Year, Month and Day columns
-    data_frame['year'] = data_frame.index.year
-    data_frame['month'] = data_frame.index.month
-    data_frame['day'] = data_frame.index.day
-    
-    # Display a random sample of 5 rows - year, month and day columsn are added
-    #print(data_frame.sample(5, random_state=0))
-    
-    # groups data by date and close - x axis is date, y axis is mean closing price
-    temp = data_frame.groupby(['Date']) ['Close'].mean()
-    
-    # plots line graph of mean closing price vs date - WORKING NOW
-    temp.plot(figsize=(15, 5), kind='line', title=f'{ticker} Closing Prices(Monthwise)', fontsize=14)
-    plt.show()
-    
-    # plots bar graph of mean closing price vs month - WORKING NOW
-    # new_temp = data_frame.groupby('month')['Close'].mean().plot.bar()
-    # plt.title('Average Closing Price By Month')
-    # plt.xlabel('Month')
-    # plt.ylabel('Stock Price')
-    # plt.show()
-    
+
+def generateCloseModel(data_frame, ticker):
+
     # split dataset into testing and training sets - This wasn't working, now has been fixed
-    limit = int(0.8*data_frame.shape[0])
+    limit = int(0.9*data_frame.shape[0])
     test = data_frame[limit:] # last 20% of values
     train = data_frame[:limit] # first 80% of values
     # Plot into graph
-    plt.plot(train.groupby(['Date']) ['Close'].mean(), color='blue', label='Train')
-    plt.plot(test.groupby(['Date']) ['Close'].mean(), color='red', label='Test')
+    plt.plot(train.groupby(['date']) ['4. close'].mean(), color='blue', label='Train')
+    plt.plot(test.groupby(['date']) ['4. close'].mean(), color='red', label='Test')
     plt.legend(loc='best')
     plt.title('Training and Test Data')
     plt.show()
-    
-    is_stationary = test_stationarity(train['Close'])
+
+    is_stationary = test_stationarity(train['4. close'])
 
     #-- SUMMARY OF LINES 118 - 194 -------------
     # If the stock prices are not stationary, we remove the trends and seasonality
-    
+
     if not is_stationary:
     # taking ln of training and testing closing values
-        train_log = np.log(train['Close'])
-        test_log = np.log(test['Close'])
-    
+        train_log = np.log(train['4. close'])
+        test_log = np.log(test['4. close'])
+
         # plotting moving average
         moving_avg = train_log.rolling(24).mean()
         # plt.plot(train_log, label='Original')
         # plt.plot(moving_avg, color='red', label='Moving Average')
         # plt.title('Moving Average')
         # plt.show()
-    
+
         # removing trend to make time series stationary
         train_log_moving_avg_diff = train_log - moving_avg
         train_log_moving_avg_diff.dropna(inplace=True)  # took average of first 24 values, so rolling mean is not defined for first 23 - drop values
-    
+
     # test_stationarity(train_log_moving_avg_diff)
-    
+
         # stabilise mean of time series - required for stationary time series
         train_log_diff = train_log - train_log.shift(1)
         test_stationarity(train_log_diff.dropna())
-    
+
 
     # fit auto ARIMA - fit model on the univariate series
-    
+
         model = auto_arima(train_log, trace=True, error_action='ignore', suppress_warnings=True)
         model.fit(train_log)
-    
+
         # predict values on validation set
         forecast = model.predict(n_periods=len(test))  # n_periods creates errors
         forecast = pd.DataFrame(forecast, index=test_log.index, columns=['Prediction'])
         forecast = np.exp(forecast) # Adjust for non-log values
-    
+
+
         # Graph forecasts vs actual
-        plt.plot(train.groupby(['Date']) ['Close'].mean(), label='Train')
-        plt.plot(test.groupby(['Date']) ['Close'].mean(), label='Test')
+        plt.plot(train.groupby(['date']) ['4. close'].mean(), label='Train')
+        plt.plot(test.groupby(['date']) ['4. close'].mean(), label='Test')
         plt.plot(forecast, color='red', label='Prediction')
         plt.title(f'{ticker} Stock Price Prediction')
         plt.xlabel('Time')
         plt.ylabel('Actual Stock Price')
         plt.legend(loc='upper left', fontsize=8)
         plt.show()
-    
+
         # check performance of model using RMSE as metric
 
         rms = np.sqrt(mean_squared_error(test_log,np.log(forecast)))
 
     else:
-        model = auto_arima(train['Close'], trace=True, error_action='ignore', suppress_warnings=True)
-        model.fit(train['Close'])
-        
+        model = auto_arima(train['4. close'], trace=True, error_action='ignore', suppress_warnings=True)
+        model.fit(train['4. close'])
+
         # predict values on validation set
         forecast = model.predict(n_periods=len(test))  # n_periods creates errors
         forecast = pd.DataFrame(forecast, index=test.index, columns=['Prediction'])
-        
+
         # Graph forecasts vs actual
-        plt.plot(train.groupby(['Date']) ['Close'].mean(), label='Train')
-        plt.plot(test.groupby(['Date']) ['Close'].mean(), label='Test')
+        plt.plot(train.groupby(['date']) ['4. close'].mean(), label='Train')
+        plt.plot(test.groupby(['date']) ['4. close'].mean(), label='Test')
         plt.plot(forecast, color='red', label='Prediction')
         plt.title(f'{ticker} Stock Price Prediction')
         plt.xlabel('Time')
         plt.ylabel('Actual Stock Price')
         plt.legend(loc='upper left', fontsize=8)
         plt.show()
-        
+
         # check performance of model using RMSE as metric
-     
+
         rms = np.sqrt(mean_squared_error(test,forecast))
-        
+
     result = rating(forecast)
     result.append(rms)
     result.append(ticker)
     return tuple(result)
-    
-        
+
+
     # print(f" You should {rating(forecast)} this stock")
 
-def HoltExp(data, ticker):
-    # inserts an entry into list of warning filters
-    warnings.filterwarnings('ignore')   
-    
-    # style of graph being plotted - font style and related preferences (can be changed)
-    plt.style.use('dark_background')
-    
-    # changing default figsize parameter
-    rcParams['figure.figsize'] = 10, 6
-    
-    data_frame = pd.read_csv(data)    # modified from example as path was not being picked up
-    
-    # gets Date column data
-    con = data_frame['Date']
-    # converts date column to a Python DateTime object
-    data_frame['Date'] = pd.to_datetime(data_frame['Date'])
-    
-    # sets data_frame index to Date column
-    data_frame.set_index('Date', inplace=True)
-    
-    # get Year, Month and Day columns
-    data_frame['year'] = data_frame.index.year
-    data_frame['month'] = data_frame.index.month
-    data_frame['day'] = data_frame.index.day
-    
-    # Display a random sample of 5 rows - year, month and day columsn are added
-    #print(data_frame.sample(5, random_state=0))
-    
-    # groups data by date and close - x axis is date, y axis is mean closing price
-    temp = data_frame.groupby(['Date']) ['Close'].mean()
-    
-    # plots line graph of mean closing price vs date - WORKING NOW
-    temp.plot(figsize=(15, 5), kind='line', title=f'{ticker} Closing Prices(Monthwise)', fontsize=14)
-    plt.show()
-    
-    # plots bar graph of mean closing price vs month - WORKING NOW
-    # new_temp = data_frame.groupby('month')['Close'].mean().plot.bar()
-    # plt.title('Average Closing Price By Month')
-    # plt.xlabel('Month')
-    # plt.ylabel('Stock Price')
-    # plt.show()
-    
+def HoltExp(data_frame, ticker):
+
     # split dataset into testing and training sets - This wasn't working, now has been fixed
-    limit = int(0.8*data_frame.shape[0])
+    limit = int(0.9*data_frame.shape[0])
     test = data_frame[limit:] # last 20% of values
     train = data_frame[:limit] # first 80% of values
     # Plot into graph
-    plt.plot(train.groupby(['Date']) ['Close'].mean(), color='blue', label='Train')
-    plt.plot(test.groupby(['Date']) ['Close'].mean(), color='red', label='Test')
+    plt.plot(train.groupby(['date']) ['4. close'].mean(), color='blue', label='Train')
+    plt.plot(test.groupby(['date']) ['4. close'].mean(), color='red', label='Test')
     plt.legend(loc='best')
     plt.title('Training and Test Data')
     plt.show()
-    
-    is_stationary = test_stationarity(train['Close'])
-    
+
+    is_stationary = test_stationarity(train['4. close'])
+
     #-- SUMMARY OF LINES 118 - 194 -------------
     # If the stock prices are not stationary, we remove the trends and seasonality
-    
+
     if not is_stationary:
         # taking ln of training and testing closing values
-        train_log = np.log(train['Close'])
-        test_log = np.log(test['Close'])
-        
+        train_log = np.log(train['4. close'])
+        test_log = np.log(test['4. close'])
+
         # plotting moving average
         moving_avg = train_log.rolling(24).mean()
         # plt.plot(train_log, label='Original')
         # plt.plot(moving_avg, color='red', label='Moving Average')
         # plt.title('Moving Average')
         # plt.show()
-        
+
         # removing trend to make time series stationary
         train_log_moving_avg_diff = train_log - moving_avg
         train_log_moving_avg_diff.dropna(inplace=True)  # took average of first 24 values, so rolling mean is not defined for first 23 - drop values
-        
+
         # test_stationarity(train_log_moving_avg_diff)
-        
+
         # stabilise mean of time series - required for stationary time series
         train_log_diff = train_log - train_log.shift(1)
         test_stationarity(train_log_diff.dropna())
-        
-        
+
+
         # fit Exponential Smoothing - fit model on the univariate series
-        
+
         model = ExponentialSmoothing(np.array(train_log), trend='mul', seasonal=None, damped=True)
         fit = model.fit()
 
@@ -287,59 +210,110 @@ def HoltExp(data, ticker):
         forecast = fit.forecast(np.asarray(len(test)))  # n_periods creates errors
         forecast = pd.DataFrame(forecast, index=test_log.index, columns=['Prediction'])
         forecast = np.exp(forecast) # Adjust for non-log values
-        
+
         # Graph forecasts vs actual
-        plt.plot(train.groupby(['Date']) ['Close'].mean(), label='Train')
-        plt.plot(test.groupby(['Date']) ['Close'].mean(), label='Test')
+        plt.plot(train.groupby(['date']) ['4. close'].mean(), label='Train')
+        plt.plot(test.groupby(['date']) ['4. close'].mean(), label='Test')
         plt.plot(forecast, color='red', label='Prediction')
         plt.title(f'{ticker} Stock Price Prediction')
         plt.xlabel('Time')
         plt.ylabel('Actual Stock Price')
         plt.legend(loc='upper left', fontsize=8)
         plt.show()
-        
+
         # check performance of model using RMSE as metric
-        
+
         rms = np.sqrt(mean_squared_error(test_log,np.log(forecast)))
-        
+
     else:
-        model = ExponentialSmoothing(np.array(train['Close']), trend='mul', seasonal=None, damped=True)
+        model = ExponentialSmoothing(np.array(train['4. close']), trend='mul', seasonal=None, damped=True)
         fit = model.fit()
 
         # predict values on validation set
         forecast = fit.forecast(len(test))  # n_periods creates errors
         forecast = pd.DataFrame(forecast, index=test.index, columns=['Prediction'])
-        
+
         # Graph forecasts vs actual
-        plt.plot(train.groupby(['Date']) ['Close'].mean(), label='Train')
-        plt.plot(test.groupby(['Date']) ['Close'].mean(), label='Test')
+        plt.plot(train.groupby(['date']) ['4. close'].mean(), label='Train')
+        plt.plot(test.groupby(['date']) ['4. close'].mean(), label='Test')
         plt.plot(forecast, color='red', label='Prediction')
         plt.title(f'{ticker} Stock Price Prediction')
         plt.xlabel('Time')
         plt.ylabel('Actual Stock Price')
         plt.legend(loc='upper left', fontsize=8)
         plt.show()
-        
+
         # check performance of model using RMSE as metric
-        
+
         rms = np.sqrt(mean_squared_error(test,forecast))
-    print(rms)
-        
+
     result = rating(forecast)
     result.append(rms)
-    result.append(ticker)    
+    result.append(ticker)
     return tuple(result)
-        
-def modelSelection(data, ticker):
-    close = generateCloseModel(data, ticker)
-    holt = HoltExp(data, ticker)
-    return sorted([close, holt], key=lambda x: x[2])[0]
+
+
+
+
+def modelSelection(ticker):
+    # inserts an entry into list of warning filters
+    warnings.filterwarnings('ignore')
     
+    # style of graph being plotted - font style and related preferences (can be changed)
+    plt.style.use('dark_background')
+    
+    # changing default figsize parameter
+    rcParams['figure.figsize'] = 10, 6
+    
+    ts = TimeSeries(key='BFNMGWERB9T9B5AO', output_format='pandas')
+    
+    
+    data_frame, meta_data = ts.get_daily(symbol=ticker, outputsize='full')
+    # pd.read_csv(data)    # modified from example as path was not being picked up
+    
+    # gets Date column data
+    # con = data_frame['date']
+    # converts date column to a Python DateTime object
+    data_frame['date'] = pd.to_datetime(data_frame.index)
+    
+    # sets data_frame index to Date column
+    data_frame.set_index('date', inplace=True)
+    
+    # get Year, Month and Day columns
+    data_frame['year'] = data_frame.index.year
+    data_frame['month'] = data_frame.index.month
+    data_frame['day'] = data_frame.index.day
+    data_frame = data_frame.sort_index(axis=0 ,ascending=True) # Reverse DataFrame
+    indexNames = data_frame[data_frame['year'] < datetime.now().year - 5].index
+    data_frame.drop(indexNames , inplace=True)
+    # Display a random sample of 5 rows - year, month and day columsn are added
+    #print(data_frame.sample(5, random_state=0))
+    
+    # groups data by date and close - x axis is date, y axis is mean closing price
+    temp = data_frame.groupby(['date']) ['4. close'].mean()
+    
+    # plots line graph of mean closing price vs date - WORKING NOW
+    temp.plot(figsize=(15, 5), kind='line', title=f'{ticker} Closing Prices(Monthwise)', fontsize=14)
+    plt.show()
+    
+    # plots bar graph of mean closing price vs month - WORKING NOW
+    # new_temp = data_frame.groupby('month')['Close'].mean().plot.bar()
+    # plt.title('Average Closing Price By Month')
+    # plt.xlabel('Month')
+    # plt.ylabel('Stock Price')
+    # plt.show()
+    
+    close = generateCloseModel(data_frame, ticker)
+    holt = HoltExp(data_frame, ticker)
+    # return(close, holt)
+    return sorted([close], key=lambda x: x[2])[0]
+
 
 
 
 # --- NEXT TASKS ---#
 # TODO: Develop other models based on volume, etc
-# TODO: Develop model evaluation
-# TODO: Adopt and migrate to stocks API
+# TODO: Async
+# TODO: API Key Encapsulation
 # TODO: Present status using InitialState
+# TODO: S&P 500 metrics
